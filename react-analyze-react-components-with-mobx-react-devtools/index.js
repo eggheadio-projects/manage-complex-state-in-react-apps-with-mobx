03a419c9fcece6c2676c878172af8f71e5cde69b
lsminter <lsminteris@gmail.com>
2018-07-19 14:47:25 -0700

    deleted unecessary files, updated index.js and package.json files

--- react-analyze-react-components-with-mobx-react-devtools/index.js
+++ react-analyze-react-components-with-mobx-react-devtools/index.js
@@ -1,25 +1,27 @@
-const {observable} = mobx;
-const {observer} = mobxReact;
-const {Component} = React;
-const Devtools = mobxDevtools.default;
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Component } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import DevTools from 'mobx-react-devtools';
 
 const appState = observable({
-  count : 0
+  count: 0
 })
-appState.increment = function() {
+appState.increment = function () {
   this.count++;
 }
-appState.decrement = function() {
+appState.decrement = function () {
   this.count--;
-  console.log('hoi') || displayInPreview('hoi')
+  console.log('hoi')
 }
 
 @observer class Counter extends Component {
   render() {
     return (
       <div>
-        <Devtools />
-        Counter: {this.props.store.count} <br/>
+        <DevTools />
+        Counter: {this.props.store.count} <br />
         <button onClick={this.handleInc}> + </button>
         <button onClick={this.handleDec}> - </button>
       </div>
@@ -29,23 +31,11 @@ appState.decrement = function() {
   handleInc = () => {
     this.props.store.increment()
   }
-  
+
   handleDec = () => {
     this.props.store.decrement()
   }
 }
 
-ReactDOM.render(
-  <Counter store={appState} />,
-  document.getElementById("app")
-)
-
-
-
-// display in plunker preview
-function displayInPreview(string) {
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode(string); 
-  newDiv.appendChild(newContent);
-  document.body.appendChild(newDiv)
-}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Counter store={appState} />, rootElement);

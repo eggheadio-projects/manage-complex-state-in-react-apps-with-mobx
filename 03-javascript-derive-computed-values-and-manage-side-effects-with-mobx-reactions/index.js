c26242b23046bcb8941e660ab730cfba38d07ca1
lsminter <lsminteris@gmail.com>
2018-07-19 15:12:45 -0700

    fixed lesson 3 files to work in sandbox

--- 03-javascript-derive-computed-values-and-manage-side-effects-with-mobx-reactions/index.js
+++ 03-javascript-derive-computed-values-and-manage-side-effects-with-mobx-reactions/index.js
@@ -1,32 +1,35 @@
-const {observable, computed} = mobx;
-const {observer} = mobxReact;
-const {Component} = React;
-const DevTools = mobxDevtools.default;
+import { observable, computed } from 'mobx';
+import { observer } from 'mobx-react';
+import { Component } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import DevTools from 'mobx-react-devtools';
 
 const t = new class Temperature {
+  // Change this unit instead of changing the unit in the console.
   @observable unit = "C";
   @observable temperatureCelsius = 25;
 
   @computed get temperatureKelvin() {
-    console.log("calculating Kelvin") || displayInPreview("calculating Kelvin");
-    return this.temperatureCelsius * (9/5) + 32
+    console.log("calculating Kelvin");
+    return this.temperatureCelsius * (9 / 5) + 32
   }
-   
+
   @computed get temperatureFahrenheit() {
-    console.log("calculating Fahrenheit") || displayInPreview("calculating Fahrenheit");
+    console.log("calculating Fahrenheit");
     return this.temperatureCelsius + 273.15
   }
-   
+
   @computed get temperature() {
-    console.log("calculating temperature") || displayInPreview("calculating temperature");
-    switch(this.unit) {
+    console.log("calculating temperature");
+    switch (this.unit) {
       case "K": return this.temperatureKelvin + "ºK"
       case "F": return this.temperatureFahrenheit + "ºF"
       case "C": return this.temperatureCelsius + "ºC"
     }
   }
 }
-   
+
 const App = observer(({ temperature }) => (
   <div>
     {temperature.temperature}
@@ -34,18 +37,6 @@ const App = observer(({ temperature }) => (
   </div>
 ))
 
-ReactDOM.render(
-  <App temperature={t} />,
-  document.getElementById("app")
-)
-   
-   
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App temperature={t} />, rootElement);
 
-
-// display in plunker preview
-function displayInPreview(string) {
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode(string); 
-  newDiv.appendChild(newContent);
-  document.body.appendChild(newDiv)
-}

27d3956391a531e6078740411b74d4695ea98317
lsminter <lsminteris@gmail.com>
2018-07-20 13:19:31 -0700

    accidently added wrong code on last commit

--- 06-react-pass-observable-data-through-props-in-mobx/index.js
+++ 06-react-pass-observable-data-through-props-in-mobx/index.js
@@ -1,57 +1,97 @@
-import { observable, computed, action, transaction } from 'mobx';
-import { observer } from 'mobx-react';
-import { Component } from 'react';
-import React from "react";
+import {
+  observable,
+  computed,
+  action,
+  transaction,
+  useStrict,
+  extendObservable,
+  asMap
+} from "mobx";
+import { observer } from "mobx-react";
+import React, { Component } from "react";
+import DevTools from "mobx-react-devtools";
 import ReactDOM from "react-dom";
-import DevTools from 'mobx-react-devtools';
 
-const t = new class Temperature {
-  // To change unit and temperature, do that here instead of in the console. 
+class Temperature {
+  id = Math.random();
   @observable unit = "C";
   @observable temperatureCelsius = 25;
 
-  @computed get temperatureKelvin() {
+  constructor(degrees, unit) {
+    this.setTemperatureAndUnit(degrees, unit);
+  }
+
+  @computed
+  get temperatureKelvin() {
     console.log("calculating Kelvin");
-    return this.temperatureCelsius * (9 / 5) + 32
+    return this.temperatureCelsius * (9 / 5) + 32;
   }
 
-  @computed get temperatureFahrenheit() {
+  @computed
+  get temperatureFahrenheit() {
     console.log("calculating Fahrenheit");
-    return this.temperatureCelsius + 273.15
+    return this.temperatureCelsius + 273.15;
   }
 
-  @computed get temperature() {
+  @computed
+  get temperature() {
     console.log("calculating temperature");
     switch (this.unit) {
-      case "K": return this.temperatureKelvin + "ºK"
-      case "F": return this.temperatureFahrenheit + "ºF"
-      case "C": return this.temperatureCelsius + "ºC"
+      case "K":
+        return this.temperatureKelvin + "ºK";
+      case "F":
+        return this.temperatureFahrenheit + "ºF";
+      case "C":
+        return this.temperatureCelsius + "ºC";
     }
   }
 
-  @action setUnit(newUnit) {
+  @action
+  setUnit(newUnit) {
     this.unit = newUnit;
   }
 
-  @action setCelsius(degrees) {
+  @action
+  setCelsius(degrees) {
     this.temperatureCelsius = degrees;
   }
 
-  @action("update temperature and unit")
+  @action
   setTemperatureAndUnit(degrees, unit) {
     this.setCelsius(degrees);
     this.setUnit(unit);
   }
+
+  @action
+  inc() {
+    this.setCelsius(this.temperatureCelsius + 1);
+  }
 }
 
-const App = observer(({ temperature }) => (
-  <div>
-    {temperature.temperature}
-    <DevTools />
-  </div>
-))
+const temps = observable([]);
+temps.push(new Temperature(20, "K"));
+temps.push(new Temperature(25, "F"));
+temps.push(new Temperature(20, "C"));
 
+const App = observer(({ temperatures }) => (
+  <ul>
+    {temperatures.map(t => <TView key={t.id} temperature={t} />)}
+    <DevTools />
+  </ul>
+));
+
+@observer
+class TView extends React.Component {
+  render() {
+    const t = this.props.temperature;
+    return <li onClick={this.onTemperatureClick}>{t.temperature}</li>;
+  }
+
+  @action
+  onTemperatureClick = () => {
+    this.props.temperature.inc();
+  };
+}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App temperature={t} />, rootElement);
-
+ReactDOM.render(<App temperatures={temps} />, rootElement);

57474ee0bfaac2589213786d8f1300567a4ec351
lsminter <lsminteris@gmail.com>
2018-07-23 12:30:01 -0700

    updated code to work with current version of dependencies

--- 09-react-write-custom-mobx-reactions-with-when-and-autorun/index.js
+++ 09-react-write-custom-mobx-reactions-with-when-and-autorun/index.js
@@ -1,8 +1,20 @@
 // noprotect
-const {observable, computed, action, transaction, useStrict, extendObservable, asMap, when, autorun} = mobx;
-const {observer, Provider} = mobxReact;
-const {Component} = React;
-const DevTools = mobxDevtools.default;
+import {
+  observable,
+  computed,
+  action,
+  transaction,
+  useStrict,
+  extendObservable,
+  asMap,
+  when,
+  autorun
+} from "mobx";
+import { observer, Provider, inject } from "mobx-react";
+import { Component } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import DevTools from "mobx-react-devtools";
 
 const APPID = "6c9bb64443d124019b41ea00de26732e"
 
@@ -19,27 +31,27 @@ class Temperature {
   }
 
   @action fetch() {
-     window.fetch(`https://api.openweathermap.org/data/2.5/weather?appid=${APPID}&q=${this.location}`)
-    .then(res => res.json()
-    .then(action(json => {
-      this.temperatureCelsius = json.main.temp -273.15
-      this.loading = false
-    })))
+    window.fetch(`https://api.openweathermap.org/data/2.5/weather?appid=${APPID}&q=${this.location}`)
+      .then(res => res.json()
+        .then(action(json => {
+          this.temperatureCelsius = json.main.temp - 273.15
+          this.loading = false
+        })))
   }
 
   @computed get temperatureKelvin() {
-    console.log("calculating Kelvin") || displayInPreview("calculating Kelvin");
-    return this.temperatureCelsius * (9/5) + 32
+    console.log("calculating Kelvin");
+    return this.temperatureCelsius * (9 / 5) + 32
   }
-   
+
   @computed get temperatureFahrenheit() {
-    console.log("calculating Fahrenheit") || displayInPreview("calculating Fahrenheit");
+    console.log("calculating Fahrenheit");
     return this.temperatureCelsius + 273.15
   }
-   
+
   @computed get temperature() {
-    console.log("calculating temperature") || displayInPreview("calculating temperature");
-    switch(this.unit) {
+    console.log("calculating temperature");
+    switch (this.unit) {
       case "K": return this.temperatureKelvin + "ºK"
       case "F": return this.temperatureFahrenheit + "ºF"
       case "C": return this.temperatureCelsius + "ºC"
@@ -53,7 +65,7 @@ class Temperature {
   @action setCelsius(degrees) {
     this.temperatureCelsius = degrees;
   }
-   
+
   @action("update temperature and unit")
   setTemperatureAndUnit(degrees, unit) {
     this.setCelsius(degrees);
@@ -100,12 +112,3 @@ autorun(() => {
     render(temps)
 })
 
-
-
-// display in plunker preview
-function displayInPreview(string) {
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode(string); 
-  newDiv.appendChild(newContent);
-  document.body.appendChild(newDiv)
-}   
